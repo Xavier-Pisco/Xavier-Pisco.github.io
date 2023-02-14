2dd05aea33d5e0b7cf4b66fddd3c3270c7d64f01
Xavier Pisco <x.pisco@gmail.com>
2023-02-14 17:24:19 +0000

    Astro config fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
